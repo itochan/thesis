ae229336a69c46840c94c30db60de09a916aa6e2
Kazunori Jo <itochan315@gmail.com>
2019-12-12 07:58:08 +0900

    Copy src from akkyie/thesis

--- src/make.sh
+++
@@ -1,3 +0,0 @@
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest dvipdfmx thesis.dvi
